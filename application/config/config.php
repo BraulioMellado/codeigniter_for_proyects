aba6e2f4c62324c1086db6b52c1139246044baf1
juliopalro <juliopalro@gmail.com>
2013-04-08 17:03:52 -0500

    configuracion de la base_url()

--- application/config/config.php
+++ application/config/config.php
@@ -1,20 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
-|--------------------------------------------------------------------------
-| Base Site URL
-|--------------------------------------------------------------------------
-|
-| URL to your CodeIgniter root. Typically this will be your base URL,
-| WITH a trailing slash:
-|
-|	http://example.com/
-|
-| If this is not set then CodeIgniter will guess the protocol, domain and
-| path to your installation.
-|
+|Para poder usar url relativas se debe de tener en cuenta
+/esta configuracion
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://localhost/codeigniter_by_proyects/';
 
 /*
 |--------------------------------------------------------------------------
