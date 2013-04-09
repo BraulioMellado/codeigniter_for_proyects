8cb6a76fc361f88fe01000de656a2e9cd4ec9af4
juliopalro <juliopalro@gmail.com>
2013-04-08 21:02:35 -0500

    agragado de CSS a la web

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,11 @@
 
-<h1>Mensage desde home</h1>
+<header id="header">
+	<ul class="menu">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Imagenes</a></li>
+		<li><a href="#">Videos</a></li>
+		<li><a href="#">Subir</a></li>
+	</ul>
+	<h1>Mensage desde home</h1>
+</header>
 <p>Hola a todos</p>
--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -4,4 +4,5 @@
 	<meta charset="utf-8">
 	<title><?= $titulo ?></title>
 	<link rel="shortcut icon" href="<?= base_url()?>imagenes/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>css/estilo.css">
 <body>
