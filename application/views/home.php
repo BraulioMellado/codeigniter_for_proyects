b7c240b8f1dfd78900ed9f8480f5238ff6f13ac6
juliopalro <juliopalro@gmail.com>
2013-04-08 21:12:46 -0500

    formalizando a html5

--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,13 @@
 
 <header id="header">
-	<ul class="menu">
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Imagenes</a></li>
-		<li><a href="#">Videos</a></li>
-		<li><a href="#">Subir</a></li>
-	</ul>
+	<nav id="nav">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Imagenes</a></li>
+			<li><a href="#">Videos</a></li>
+			<li><a href="#">Subir</a></li>
+		</ul>
+	</nav>
 	<h1>Mensage desde home</h1>
 </header>
 <p>Hola a todos</p>
