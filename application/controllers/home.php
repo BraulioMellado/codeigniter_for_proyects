4ae94f24cc7ada61acb74e65ab9d3a279087e429
juliopalro <juliopalro@gmail.com>
2013-04-10 13:14:57 -0500

    estableciendo plantilla con css

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,6 @@ class Home extends CI_Controller {
 		$titulo['titulo']=ucfirst('titulo de la pagina');
 		$this->load->view('layout/head',$titulo);
 		$this->load->view('home');
-		$this->load->view('layout/foot');
+		$this->load->view('layout/footer');
 	}
 }
---
+++ application/views/layout/footer.php
@@ -0,0 +1,16 @@
+
+
+<footer id="footer">
+	<div class="caja">
+		<h3>Nuestros clientes</h3>
+		<div class="clientes">
+			<p><a href="#">Nike</a></p>
+			<p><a href="#">Volvo</a></p>
+			<p><a href="#">Playboy</a></p>
+			<p><a href="#">Yo soy</a></p>
+			<p><a href="#">Rojo Fama Contra Fama</a></p>
+	</div>
+	</div>
+</footer>
+</body>
+</html>
